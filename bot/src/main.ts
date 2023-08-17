ffc4d38cce44f1805e5915fa1e58554e6a808820
Oachi <ongsa.srp.mkm@gmail.com>
2023-08-17 13:59:46 +0700

    copy code from old project - migrate to discord.js v14

--- bot/src/main.ts
+++ bot/src/main.ts
@@ -1,1 +1,33 @@
-console.log("E")
+import { ActivityType, Client, GatewayIntentBits, Partials } from "discord.js"
+import { config } from "dotenv"
+
+config()
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.Guilds
+    ],
+})
+
+client.on('ready', () => {
+    if (process.env.device === 'development machine') {
+        const presence = client.user!.setActivity('rewrite becuase I can', { type: ActivityType.Watching })
+        console.log(`Activity set to ${presence.activities[0].name}`)
+    } 
+    console.log('[main] Ready')
+})
+
+client.on('interactionCreate', async (interaction) => {
+    interaction
+})
+
+process.on('SIGINT', () => {
+    // do something
+    client.destroy()
+    console.log('destroyed client')
+    process.exit();
+})
+
+client.login(process.env.CLIENT_TOKEN);
